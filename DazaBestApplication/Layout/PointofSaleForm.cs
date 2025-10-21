b5cd1d442ae1041038d48eb926cb4982cd8aa695
Julliebert <cogueronjulliebert21@gmail.com>
2025-10-21 23:16:15 +0800

    Database Updated -Updated Version-

--- DazaBestApplication/Layout/PointofSaleForm.cs
+++ DazaBestApplication/Layout/PointofSaleForm.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
 using SystemBackEnd.Models;
 using SystemBackEnd.ServiceModels;
 using SystemBackEnd.Services;
-using static TheArtOfDev.HtmlRenderer.Adapters.RGraphicsPath;
+using SystemBackEnd.EventHandlers;
 
 namespace DazaBestApplication.Layout
 {
@@ -271,6 +271,22 @@ namespace DazaBestApplication.Layout
                 Backgroundmodal.Dispose();
             }
         }
+        //Reset or New Transaction
+        private async Task NewTransaction()
+        {
+            CurrentOrders.Clear();
+            ProductOrdersDatagrid.Rows.Clear();
+            discountPercentage = 0;
+            Subtotalvalue.Text = "$0.00";
+            TotalValue.Text = "$0.00";
+            Total = 0;
+
+            //Reset POSTransactionDone
+            POSTransactionDone = new POSTransactionDone
+            {
+                TransactionBy = Guid.NewGuid() // Replace with actual user ID
+            };
+        }
 
 
         //Main Load
@@ -282,6 +298,12 @@ namespace DazaBestApplication.Layout
 
             //Initialize POSTransactionDone
             POSTransactionDone.TransactionBy = Guid.NewGuid(); // Replace with actual user ID
+
+            //Hook Payment Event Handler
+            POSEventHandler.PaymentTransactionSuccess += async () =>
+            {
+                await NewTransaction();
+            };
         }
 
 
@@ -343,6 +365,14 @@ namespace DazaBestApplication.Layout
         private async void PaymentButton_Click(object sender, EventArgs e)
         {
             await ShowPaymentModal();
+        }
+        //Handle Cancel or Reset Order Button Click
+        private async void CancelResetOrderButton_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure you want to cancel the current order?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                await NewTransaction();
+            }
         }
     }
 
--- DazaBestApplication/Modals/POSPaymentModal.cs
+++ DazaBestApplication/Modals/POSPaymentModal.cs
@@ -8,22 +8,144 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SystemBackEnd.ServiceModels;
+using SystemBackEnd.Services;
+using SystemBackEnd.Models;
+using SystemBackEnd;
+using Bunifu.UI.WinForms;
+using SystemBackEnd.EventHandlers;
 
 namespace DazaBestApplication.Modals
 {
     public partial class POSPaymentModal : Form
     {
         private POSTransactionDone ProcessTransaction = new POSTransactionDone();
+        private decimal AmountRecieved = 0;
+        private decimal ChangeAmount = 0;
 
         public POSPaymentModal(POSTransactionDone _paymentprocess)
         {
             InitializeComponent();
             ProcessTransaction = _paymentprocess;
         }
+
+        
         //Load Event
         private void POSPaymentModal_Load(object sender, EventArgs e)
         {
-            Totalvaluetext.Text = ProcessTransaction.Grandtotal.ToString("N2");
+            Totalvaluetext.Text = ProcessTransaction.Total.ToString("N2");
+            PaymentRecievedtextbox.Focus();
+            PaymentRecievedtextbox.Text = AmountRecieved.ToString("N2");
+        }
+        
+        //Process Transaction
+        private async Task<bool> PayingTransaction()
+        {
+            bool isPaymentSuccess = false;
+            try
+            {
+                ProcessTransaction.PaymentAmount = AmountRecieved;
+                ProcessTransaction.Grandtotal = ProcessTransaction.Total;
+                ProcessTransaction.Total = ProcessTransaction.Total;
+                ProcessTransaction.PaymentMethod = "CASH";
+                ProcessTransaction.Discount = 0;
+
+                POSService posService = new POSService(new BackEndDBContext());
+                isPaymentSuccess = await posService.ProcessPOSTransaction(ProcessTransaction);
+            }
+            catch (Exception e)
+            {
+
+            }
+            return isPaymentSuccess;
+
+        }
+        //Pay Transaction With Validation
+        private async Task PayTransaction()
+        {
+            try
+            {
+                if (ChangeAmount >= 0)
+                {
+                    bool PaymentProcessSuccess = await PayingTransaction();
+                    if (PaymentProcessSuccess)
+                    {
+                        await POSEventHandler.InvokePaymentTransactionSuccess();
+                        MessageBox.Show("Payment Success", "System", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        this.Close();
+                        this.DialogResult = DialogResult.No;
+                    }
+                    else
+                    {
+                        MessageBox.Show("System Error", "System", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Insufficient Payment Amount", "System", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+        }
+        //Close Modal
+        private void CloseModal()
+        {
+            this.Close();
+            this.DialogResult = DialogResult.No;
+        }
+        //Calculate Change Amount
+        private async Task CalculateChangeAmount()
+        {
+            try
+            {
+                ChangeAmount = AmountRecieved - ProcessTransaction.Total;
+                Changelabel.Text = ChangeAmount.ToString("N2");
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+
+
+
+        //Payment Recieved Key Press Event
+        private void PaymentRecievedtextbox_TextChange(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsControl(e.KeyChar))
+                return;
+
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+                return;
+            }
+
+            TextBox textBox = sender as TextBox;
+            if (e.KeyChar == '.' && textBox.Text.Contains('.'))
+            {
+                e.Handled = true;
+            }
+        }
+        //Payment OK Button Click Event
+        private void PaymentOkButton_Click(object sender, EventArgs e)
+        {
+            PayTransaction();
+        }
+        //Close Button Click Event
+        private void bunifuImageButton1_Click(object sender, EventArgs e)
+        {
+            CloseModal();
+        }
+        //Payment Recieved Text Change Event
+        private void PaymentRecievedtextbox_TextChange(object sender, EventArgs e)
+        {
+            decimal.TryParse(PaymentRecievedtextbox.Text, out AmountRecieved);
+            CalculateChangeAmount();
         }
     }
 }
